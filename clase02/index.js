acbf80fae5bb05521d557805c7d0a969ee3375d6
estebanzarate <estebanzaratedev@gmail.com>
2023-01-21 12:31:16 -0300

    challenge class 02

---
+++ clase02/index.js
@@ -0,0 +1,54 @@
+class ProducManager {
+	constructor() {
+		this.products = [];
+	}
+	addProduct = ({ title, description, price, thumbnail, code, stock }) => {
+		const codeExists = this.products.some(product => product.code === code);
+		if (codeExists) return console.warn('[!] Code already exists');
+		const id = this.products.length === 0 ? 1 : this.products[this.products.length - 1].id + 1;
+		this.products.push({ id, title, description, price, thumbnail, code, stock });
+	};
+	getProducts = () => this.products;
+	getProductById = id => this.products.find(product => product.id === id) || '[!] Not Found';
+}
+
+// Se creará una instancia de la clase “ProductManager
+const productManager = new ProducManager();
+
+// Se llamará “getProducts” recién creada la instancia, debe devolver un arreglo vacío []
+console.log(productManager.getProducts());
+
+// Se llamará al método “addProduct” con los campos:
+// title: “producto prueba”
+// description:”Este es un producto prueba”
+// price:200,
+// thumbnail:”Sin imagen”
+// code:”abc123”,
+// stock:25
+productManager.addProduct({
+	title: 'producto prueba',
+	description: 'Este es un producto prueba',
+	price: 200,
+	thumbnail: 'Sin imagen',
+	code: 'abc123',
+	stock: 25
+});
+
+// El objeto debe agregarse satisfactoriamente con un id generado automáticamente SIN REPETIRSE
+
+// Se llamará el método “getProducts” nuevamente, esta vez debe aparecer el producto recién agregado
+console.error(productManager.getProducts());
+
+// Se llamará al método “addProduct” con los mismos campos de arriba, debe arrojar un error porque el código estará repetido.
+productManager.addProduct({
+	title: 'producto prueba',
+	description: 'Este es un producto prueba',
+	price: 200,
+	thumbnail: 'Sin imagen',
+	code: 'abc123',
+	stock: 25
+});
+
+// Se evaluará que getProductById devuelva error si no encuentra el producto o el producto en caso de encontrarlo
+console.log(productManager.getProductById(1));
+console.error(productManager.getProductById(3));
