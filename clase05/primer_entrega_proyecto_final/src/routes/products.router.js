dce879a11ce8b8be2c68c15eaa015eb1069dfade
estebanzarate <estebanzaratedev@gmail.com>
2023-02-13 11:16:20 -0300

    [clase 05] add product

--- clase05/primer_entrega_proyecto_final/src/routes/products.router.js
+++ clase05/primer_entrega_proyecto_final/src/routes/products.router.js
@@ -17,7 +17,10 @@ router.get('/:pid', async (req, res) => {
 	res.status(200).json(product);
 });
 
-router.post('/', async (req, res) => {});
+router.post('/', async (req, res) => {
+	await productManager.addProduct(req.body);
+	res.status(200).json({ message: 'Product added successfully' });
+});
 
 router.put('/:pid', async (req, res) => {});
 
