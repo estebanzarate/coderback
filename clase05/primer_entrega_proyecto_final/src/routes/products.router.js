4e40c3e70887a6fd2629ad07a2c8d9a5065a101d
estebanzarate <estebanzaratedev@gmail.com>
2023-02-13 11:33:44 -0300

    [clase 05] delete product

--- clase05/primer_entrega_proyecto_final/src/routes/products.router.js
+++ clase05/primer_entrega_proyecto_final/src/routes/products.router.js
@@ -18,6 +18,9 @@ router.get('/:pid', async (req, res) => {
 });
 
 router.post('/', async (req, res) => {
+	const { title, description, code, price, stock, category } = req.body;
+	if (!title || !description || !code || !price || !stock || !category)
+		return res.status(400).json({ message: '[!] All fields are required' });
 	await productManager.addProduct(req.body);
 	res.status(200).json({ message: 'Product added successfully' });
 });
@@ -27,6 +30,9 @@ router.put('/:pid', async (req, res) => {
 	res.status(200).json({ message: 'Product updated successfully' });
 });
 
-router.delete('/:pid', async (req, res) => {});
+router.delete('/:pid', async (req, res) => {
+	await productManager.deleteProduct(Number(req.params.pid));
+	res.status(200).json({ message: 'Product deleted successfully' });
+});
 
 module.exports = router;
